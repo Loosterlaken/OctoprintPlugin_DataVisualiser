ca1b6753672c03f349f41a693ed4f352186e6820
Loosterlaken <113505369+Loosterlaken@users.noreply.github.com>
2023-05-25 20:49:54 +0200

    Setup

---
+++ Octoprint plugin_Example.py
@@ -0,0 +1,26 @@
+# Octoprint DIY plugin builder library
+import octoprint.plugin
+
+class DataVisualiser( octoprint.plugin.StartupPlugin,
+                            octoprint.plugin.TemplatePlugin):
+    def get_template_configs(self):
+        return [
+            dict(type="tab", name="Graph", template="myplugin_first_tab.jinja2", custom_bindings=True)
+        ]
+    
+    def on_after_startup(self):
+        # data  opslag locatie
+        #log = open("d:\Log.txt","a")
+        while True:
+            self._logger.info("Hij doet nu iets")
+
+    
+    
+    #def on_event(Startup): #Event should become "PrintStarted"
+    #    while True:
+
+__plugin_name__ = "Particulate matter measurement"
+__plugin_version__ = "1.0.0"
+__plugin_description__ = "SDS011 sensor setup and visualisation in tab"
+__plugin_pythoncompat__ = ">=3.7,<4"
+__plugin_implementation__ = DataVisualiser()
