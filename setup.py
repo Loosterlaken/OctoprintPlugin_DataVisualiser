b193a9f501a5ed5bd6cba87f9351c1df89777972
Loosterlaken <113505369+Loosterlaken@users.noreply.github.com>
2023-06-10 22:00:15 +0200

    sdfgfdafvav

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup
+import octoprint_setuptools
 
 plugin_identifier = "data_visualiser"
 plugin_package = "Octoprint_Data_Visualiser"
@@ -14,17 +15,6 @@ plugin_additional_data = []
 plugin_additional_packages = []
 plugin_ignored_packages = []
 additional_setup_parameters = {}
-
-from setuptools import setup
-
-try:
-	import octoprint_setuptools
-except:
-	print("Could not import OctoPrint's setuptools, are you sure you are running that under "
-							"the same python installation that OctoPrint is installed under?")
-	import sys
-
-	sys.exit(-1)
 
 setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
 	identifier=plugin_identifier,
